616cb2600351ed6d52a42d289ebdaf39810e0c06
evaroderick23 <evaroderick23@gmail.com>
2024-10-30 13:18:19 -0300

    Fixed script and organized hierarchy

--- Assets/Scripts/LightTrigger.cs
+++ Assets/Scripts/LightTrigger.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class LightTrigger : MonoBehaviour
 {
-    public GameObject sparkle;
+    public GameObject light;
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("tRIIGER");
-        sparkle.SetActive(false);
+        light.SetActive(false);
     }
 
     private void OnTriggerExit(Collider other)
     {
-        Debug.Log("Exit");
-        sparkle.SetActive(true);
+        light.SetActive(true);
     }
 }
